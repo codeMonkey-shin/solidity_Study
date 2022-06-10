b557082e1ca8d7120f6688817257337a3bb7a353
codeMonkey-shin <sdw0711@hanmail.net>
2022-06-10 16:52:58 +0900

    RSP 개선

--- RSP.sol
+++ RSP.sol
@@ -1,49 +1,53 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract RSP {
-    constructor () payable {}
+contract RPS {
+    constructor() payable {}
 
-    enum Hand {// 가위/바위/보 값에 대한 enum!
-        rock, paper, scissors
+    enum Hand {
+        rock,
+        paper,
+        scissors
     }
 
-    enum PlayerStatus {// 플레이어의 상태
-        STATUS_WIN, STATUS_LOSE, STATUS_TIE, STATUS_PENDING
+    enum PlayerStatus {
+        STATUS_WIN,
+        STATUS_LOSE,
+        STATUS_TIE,
+        STATUS_PENDING
     }
 
-    enum GameStatus {// 게임의 상태
-        STATUS_NOT_STARTED, STATUS_STARTED, STATUS_COMPLETE, STATUS_ERROR
+    enum GameStatus {
+        STATUS_NOT_STARTED,
+        STATUS_STARTED,
+        STATUS_COMPLETE,
+        STATUS_ERROR
     }
 
     struct Player {
-        address payable addr;  // 주소
-        uint256 playerBetAmount;  // 베팅 금액
-        Hand hand;  // 플레이어가 낸 가위/바위/보 값
-        PlayerStatus playerStatus;  // 사용자의 현 상태
+        address payable addr;
+        uint256 playerBetAmount;
+        bytes32 hand;
+        PlayerStatus playerStatus;
+        uint result;
+        uint count;
     }
 
     struct Game {
-        Player originator;  // 방장 정보
-        Player taker;  // 참여자 정보
-        uint256 betAmount;  // 총 베팅 금액
-        GameStatus gameStatus; // 게임의 현 상태
+        Player originator;
+        Player taker;
+        uint256 betAmount;
+        GameStatus gameStatus;
     }
 
-    mapping(uint => Game) rooms; // rooms[0], rooms[1] 형식으로 접근할 수 있으며, 각 요소는 Game 구조체 형식입니다.
-    uint roomLen = 0; // rooms의 키 값입니다. 방이 생성될 때마다 1씩 올라갑니다.
+    mapping(uint256 => Game) rooms;
+    uint256 roomLen = 0;
 
-    modifier isValidHand (Hand _hand) {
-        require((_hand  == Hand.rock) || (_hand  == Hand.paper) || (_hand == Hand.scissors));
-        _;
+    function keccak(uint256 _hand, string memory _key) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_hand, _key));
     }
 
-    modifier isPlayer (uint roomNum, address sender) {
-        require(sender == rooms[roomNum].originator.addr || sender == rooms[roomNum].taker.addr);
-        _;
-    }
-
-    function createRoom(Hand _hand) public payable returns (uint roomNum) {
+    function createRoom(bytes32 _hand) public payable returns (uint256 roomNum) {
         rooms[roomLen] = Game({
         betAmount : msg.value,
         gameStatus : GameStatus.STATUS_NOT_STARTED,
@@ -51,55 +55,92 @@ contract RSP {
         hand : _hand,
         addr : payable(msg.sender),
         playerStatus : PlayerStatus.STATUS_PENDING,
-        playerBetAmount : msg.value
+        playerBetAmount : msg.value,
+        result : 0,
+        count : 0
         }),
         taker : Player({
-        hand : Hand.rock,
+        hand : 0,
         addr : payable(msg.sender),
         playerStatus : PlayerStatus.STATUS_PENDING,
-        playerBetAmount : 0
+        playerBetAmount : 0,
+        result : 0,
+        count : 0
+
         })
         });
-        roomNum = roomLen; // 현재 방 번호를 roomNum에 할당시켜 반환
-        roomLen = roomLen+1;  // 다음 방 번호를 설정
-        // roomNum은 리턴된다.
+        roomNum = roomLen;
+        roomLen = roomLen + 1;
     }
 
-    function joinRoom(uint roomNum, Hand _hand) public payable isValidHand( _hand) {
+    function joinRoom(uint256 roomNum, bytes32 _hand) public payable {
+        require(msg.value == rooms[roomNum].betAmount);
         rooms[roomNum].taker = Player({
-        hand: _hand,
-        addr: payable(msg.sender),
-        playerStatus: PlayerStatus.STATUS_PENDING,
-        playerBetAmount: msg.value
+        hand : _hand,
+        addr : payable(msg.sender),
+        playerStatus : PlayerStatus.STATUS_PENDING,
+        playerBetAmount : msg.value,
+        result : 0,
+        count : 0
+
         });
         rooms[roomNum].betAmount = rooms[roomNum].betAmount + msg.value;
     }
 
-    function compareHands(uint roomNum) private{
-        uint8 originator = uint8(rooms[roomNum].originator.hand);
-        uint8 taker = uint8(rooms[roomNum].taker.hand);
+
+    function useOriginator(uint256 _roomNum, uint256 _hand, string memory _secretKey) public {
+        require(msg.sender == rooms[_roomNum].originator.addr);
+        require(rooms[_roomNum].originator.hand == keccak(_hand, _secretKey));
+        rooms[_roomNum].originator.result = _hand;
+        rooms[_roomNum].originator.count = 1;
+    }
+
+    function useTaker(uint256 _roomNum, uint256 _hand, string memory _secretKey) public {
+        require(msg.sender == rooms[_roomNum].taker.addr);
+        require(rooms[_roomNum].taker.hand == keccak(_hand, _secretKey));
+        rooms[_roomNum].taker.result = _hand;
+        rooms[_roomNum].taker.count = 1;
+    }
+
+
+    function compareHands(uint256 roomNum) private {
+        require(rooms[roomNum].taker.count == 1 && rooms[roomNum].originator.count == 1);
+        uint8 originator = uint8(rooms[roomNum].originator.result);
+        uint8 taker = uint8(rooms[roomNum].taker.result);
 
         rooms[roomNum].gameStatus = GameStatus.STATUS_STARTED;
 
-        if (taker == originator){ // 비긴 경우
+        if (taker == originator) {
             rooms[roomNum].originator.playerStatus = PlayerStatus.STATUS_TIE;
             rooms[roomNum].taker.playerStatus = PlayerStatus.STATUS_TIE;
-        }
-        else if ((taker +1) % 3 == originator) { // 방장이 이긴 경우
+        } else if ((taker + 1) % 3 == originator) {
             rooms[roomNum].originator.playerStatus = PlayerStatus.STATUS_WIN;
             rooms[roomNum].taker.playerStatus = PlayerStatus.STATUS_LOSE;
-        }
-        else if ((originator + 1)%3 == taker){  // 참가자가 이긴 경우
+        } else if ((originator + 1) % 3 == taker) {
             rooms[roomNum].originator.playerStatus = PlayerStatus.STATUS_LOSE;
             rooms[roomNum].taker.playerStatus = PlayerStatus.STATUS_WIN;
-        } else {  // 그 외의 상황에는 게임 상태를 에러로 업데이트한다
+        } else {
             rooms[roomNum].gameStatus = GameStatus.STATUS_ERROR;
         }
     }
 
-    function payout(uint roomNum) public payable {
-        if (rooms[roomNum].originator.playerStatus == PlayerStatus.STATUS_TIE && rooms[roomNum].taker.playerStatus == PlayerStatus.STATUS_TIE) {
-            rooms[roomNum].originator.addr.transfer(rooms[roomNum].originator.playerBetAmount);
+    modifier isPlayer(uint256 roomNum, address sender) {
+        require(
+            sender == rooms[roomNum].originator.addr ||
+            sender == rooms[roomNum].taker.addr
+        );
+        _;
+    }
+
+    function payout(uint256 roomNum) public payable isPlayer(roomNum, msg.sender) {
+        compareHands(roomNum);
+        if (
+            rooms[roomNum].originator.playerStatus == PlayerStatus.STATUS_TIE &&
+            rooms[roomNum].taker.playerStatus == PlayerStatus.STATUS_TIE
+        ) {
+            rooms[roomNum].originator.addr.transfer(
+                rooms[roomNum].originator.playerBetAmount
+            );
             rooms[roomNum].taker.addr.transfer(rooms[roomNum].taker.playerBetAmount);
         } else {
             if (rooms[roomNum].originator.playerStatus == PlayerStatus.STATUS_WIN) {
@@ -107,16 +148,14 @@ contract RSP {
             } else if (rooms[roomNum].taker.playerStatus == PlayerStatus.STATUS_WIN) {
                 rooms[roomNum].taker.addr.transfer(rooms[roomNum].betAmount);
             } else {
-                rooms[roomNum].originator.addr.transfer(rooms[roomNum].originator.playerBetAmount);
-                rooms[roomNum].taker.addr.transfer(rooms[roomNum].taker.playerBetAmount);
+                rooms[roomNum].originator.addr.transfer(
+                    rooms[roomNum].originator.playerBetAmount
+                );
+                rooms[roomNum].taker.addr.transfer(
+                    rooms[roomNum].taker.playerBetAmount
+                );
             }
+            rooms[roomNum].gameStatus = GameStatus.STATUS_COMPLETE;
         }
-        rooms[roomNum].gameStatus = GameStatus.STATUS_COMPLETE; // 게임이 종료되었으므로 게임 상태 변경
     }
-
-
-
-
-
-
 }
